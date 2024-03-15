a4b6bda7b9415e9a5503b79b0b9454fef039010b
Dip Kishor Regmi <dipkishor9910@gmail.com>
2024-03-15 22:53:36 +0545

    added Itemmodel

---
+++ EcommerceDotnet.Models/ItemModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceDotnet.Models
+{
+	public class ItemModel
+	{
+		//todo: data annotation
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public float Price { get; set; }
+		public float Discount { get; set; }
+		public string ImageURL { get; set; }
+		public bool IsDiscountPct { get; set; }
+		public bool IsPublished { get; set; }
+		public bool DisplayInHomePage { get; set; }
+	}
+}
