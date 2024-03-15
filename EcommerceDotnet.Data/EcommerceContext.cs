65c396352fd1a56bd7e883a835f3cbae0edffbab
Dip Kishor Regmi <dipkishor9910@gmail.com>
2024-03-15 22:51:09 +0545

    Added DbContext

---
+++ EcommerceDotnet.Data/EcommerceContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceDotnet.Data
+{
+	public class EcommerceContext:DbContext
+	{
+		public EcommerceContext(DbContextOptions<EcommerceContext> options)
+	   : base(options)
+		{
+		}
+
+		
+	}
+}
