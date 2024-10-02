e4898eda3ce7267d428d3c166ed3c9b098a78bd1
Dip Kishor Regmi <dipkishor9910@gmail.com>
2024-10-03 00:06:54 +0545

    ProductName Field added

--- EcommerceDotnet.Data/Migrations/20241002170522_addedProductNameFieldInCheckOutModel.Designer.cs
+++ EcommerceDotnet.Data/Migrations/20241002170522_addedProductNameFieldInCheckOutModel.Designer.cs
@@ -3,6 +3,7 @@ using EcommerceDotnet.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EcommerceDotnet.Data.Migrations
 {
     [DbContext(typeof(EcommerceContext))]
-    partial class EcommerceContextModelSnapshot : ModelSnapshot
+    [Migration("20241002170522_addedProductNameFieldInCheckOutModel")]
+    partial class addedProductNameFieldInCheckOutModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EcommerceDotnet.Data/Migrations/20241002170522_addedProductNameFieldInCheckOutModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcommerceDotnet.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedProductNameFieldInCheckOutModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProductName",
+                table: "CheckoutItems",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductName",
+                table: "CheckoutItems");
+        }
+    }
+}
