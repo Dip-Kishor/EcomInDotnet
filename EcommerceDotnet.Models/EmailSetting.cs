53077e4ca71fd0850a83e088c0a292063ee84edc
Dip Kishor Regmi <dipkishor9910@gmail.com>
2024-10-20 16:48:55 +0545

    Added new Email model

---
+++ EcommerceDotnet.Models/EmailSetting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceDotnet.Models
+{
+    public class EmailSettings
+    {
+        public string SmtpServer { get; set; }
+        public int SmtpPort { get; set; }
+        public string SmtpUser { get; set; }
+        public string SmtpPass { get; set; }
+    }
+
+}
